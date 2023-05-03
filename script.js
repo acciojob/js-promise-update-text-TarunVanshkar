c22eb8b2e94fc93a05909f87991b8c6765860df0
TarunVanshkar <117818351+TarunVanshkar@users.noreply.github.com>
2023-05-03 23:01:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("Hello, world!")
+    }, 1000)
+})
+
+const div = document.getElementById("output")
+
+promise.then((result) => {
+    div.innerText = result
+})
